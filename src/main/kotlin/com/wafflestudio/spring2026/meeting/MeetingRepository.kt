4d996c4224feec4a2043611586da15c29cc8252d
오수현 <sh020119@snu.ac.kr>
2026-09-22 18:04:42 +0900

    chore: 빈 이름 우회를 걷어내며 남은 import 제거

--- src/main/kotlin/com/wafflestudio/spring2026/meeting/MeetingRepository.kt
+++ src/main/kotlin/com/wafflestudio/spring2026/meeting/MeetingRepository.kt
@@ -1,7 +1,6 @@
 package com.wafflestudio.spring2026.meeting
 
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
 
 interface MeetingRepository : CrudRepository<Meeting, Long> {
     fun findAllBySeminarIdOrderByScheduledAt(seminarId: Long): List<Meeting>
